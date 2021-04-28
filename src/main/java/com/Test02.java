d77dc87ededc6a2d6f30204609212c0db8f1e857
dankai <email>
2021-04-28 22:11:29 +0800

    push commit

--- src/main/java/com/Test02.java
+++ src/main/java/com/Test02.java
@@ -3,5 +3,6 @@ package com;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("测试能否将工程打包到GitHub上面");
+        System.out.println("ssh-github test01");
     }
 }
